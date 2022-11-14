57e36751a5dfa6544c50538c8b6cbc8b22cfe828
GitLukeW <gitlukew@gmail.com>
2022-11-14 14:46:27 -0500

    found a bug in day one and got it fixed

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,3 +1,5 @@
+import itertools
+
 list = [10, 15, 3, 7]
 k = 17
 
@@ -5,8 +7,18 @@ k = 17
 def add_nums(list, k):
     for x in list:
         for y in list:
-            if x + y == k:
+            if x + y == k and x != y:
                 return True
 
 
 print(add_nums(list, k))
+
+
+def iter(list, k):
+    for x in itertools.combinations(list, 2):
+        print(x)
+        if sum(x) == k:
+            return True
+
+
+print(iter(list, k))
