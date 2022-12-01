6a7143c44277dc5f18fc81f974093cdefdeb7f89
GitLukeW <gitlukew@gmail.com>
2022-12-01 16:19:24 -0500

    day4

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,13 +1,25 @@
-test = [3, 4, -1, 1]
-# output == 2
+import functools
 
-sorted_test = sorted(test)
-print(sorted_test)
 
-for num in sorted_test:
-    check = 1
-    if num != check:
-        check += 1
-    else:
-        check += 0
-print(check)
+def product(accumulated_array, next_value):
+    accumulated_array.append(accumulated_array[-1] * next_value)
+    return accumulated_array
+
+
+def main(input):
+    result = functools.reduce(product, input, [1])
+    result.pop()
+
+    running_product_from_right = 1
+    for i in range(1, len(result) + 1):
+        result[-i] = result[-i] * running_product_from_right
+        running_product_from_right = running_product_from_right * input[-i]
+        return result
+
+
+# tests
+print(main([5, 1, 4, 3, 6]) == [72, 360, 90, 120, 60])
+# with one zero (all but the zero will be 0)
+print(main([5, 1, 0, 3, 6]) == [0, 0, 90, 0, 0])
+# with multiple zeros (all will be 0)
+print(main([100, 0, 1, 3, 9, 0, 55]) == [0, 0, 0, 0, 0, 0, 0])
