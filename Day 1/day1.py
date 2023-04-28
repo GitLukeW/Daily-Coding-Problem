101cb4a5ec192fc98c727b3a9b40aa325c1cf523
GitLukeW <gitlukew@gmail.com>
2023-04-28 13:18:59 -0400

    day 1

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,24 +1,62 @@
 import itertools
 
-list = [10, 15, 3, 7]
-k = 17
+
+def sumnums(lst, k):
+    for num in lst:
+        for num2 in lst:
+            if num + num2 == k:
+                return True
+    return False
 
 
-def add_nums(list, k):
-    for x in list:
-        for y in list:
-            if x + y == k and x != y:
+#print(sumnums([10, 15, 3, 7], 17))
+
+def sumnums2(lst, k):
+    for num in lst:
+        if k - num in lst:
+            return True
+    return False
+
+
+#print(sumnums2([10, 15, 3, 7], 17))
+
+def sumnums3(lst, k):
+    return any(k - num in lst for num in lst)
+
+
+print(sumnums3([10, 15, 3, 7], 17))
+
+# Bonus: Can you do this in one pass?
+
+
+def sumnums4(lst, k):
+    seen = set()
+    for num in lst:
+        if k - num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+print(sumnums4([10, 15, 3, 7], 17))
+
+
+def sumnums5(lst, k):
+    return any(k - num in lst[:i] for i, num in enumerate(lst))
+
+
+print(sumnums5([10, 15, 3, 7], 17))
+
+
+def sumnums6(lst, k):
+    for num in range(len(lst)):
+        for num2 in range(len(lst)):
+            if num != num2 and lst[num] + lst[num2] == k:
                 return True
 
-
-print(add_nums(list, k))
-
-
-def iter(list, k):
-    for x in itertools.combinations(list, 2):
-        print(x)
-        if sum(x) == k:
-            return True
+    return False
 
 
-print(iter(list, k))
+print(sumnums6([10, 15, 3, 7], 17), "5")
+print(sumnums6([11, 1, 2, 5], 17), "5")
+
